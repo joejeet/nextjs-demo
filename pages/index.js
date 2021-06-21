00035f694cd842a8ef2ce6c84e1a348c7866ab3f
Joe Singh <joe.nayyar@gmail.com>
2021-06-21 07:25:19 +0100

    Added Demo text

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Global Solutions</a>
+          Welcome to <a href="https://nextjs.org">Global Solutions Demo</a>
         </h1>
 
         <p className={styles.description}>
